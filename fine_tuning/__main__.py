0fc50a74ae03d621b98c207b92843af3d3f8b286
suehtemorp <javier.solanosaltachin@ucr.ac.cr>
2025-06-20 17:41:41 -0600

    Added fine tuning commands. Debugging begins

--- fine_tuning/__main__.py
+++ fine_tuning/__main__.py
@@ -1,5 +1,5 @@
 # Tools
-from .commands import download_test_model, eval_model
+from .commands import download_test_model, eval_model, train_model_custom_args, train_model_presaved_args
 
 # Argument parsing
 import argparse
@@ -22,7 +22,6 @@ def main():
         help='Path to directory to downloade pre-trained model into')
     download_parser.add_argument('--test', action='store_true',
         help="After downloading pre-trained model, test it running a simple prompt")
-    
 
     # - Evaluate model on a test split of the dataset
     evaluate_parser = sub_parsers.add_parser("evaluate", 
@@ -38,25 +37,77 @@ def main():
     evaluate_parser.add_argument('output_dir', type=str, 
         help='Path to directory to place resulting statistic files into')
     
+    # - Train model on dataset
+    train_parser = sub_parsers.add_parser("train", 
+        help="Train model on training and validation split of a dataset")
+    
+    # - - Register arguments
+    train_parser.add_argument('model_path', type=str, 
+        help='Path to directory with pre-trained model (might be fine-tuned)')
+    train_parser.add_argument('dataset_dir', type=str, 
+        help='Path to directory with dataset files and training/validation splits')
+    train_parser.add_argument('output_dir', type=str, 
+        help='Path to directory to save model into after training')
+    train_parser.add_argument('--stats_dir', type=str, required=False, default=None,
+        help='Path to directory to save training statistics into')
+    
+    # - - Register training command training argument variants
+    train_subparsers = train_parser.add_subparsers(dest='training_argument_source', required=True,
+        help="Specify arguments for training model")  
+    
+    # - - - Manually specify training arguments
+    train_custom_args = train_subparsers.add_parser("custom", 
+        help="Specify custom training arguments, by hand")
+    
+    # - - - - Register arguments
+    train_custom_args.add_argument('checkpoints_dir', type=str,
+        help='Path to directory to save training checkpoints into')
+    train_custom_args.add_argument('training_epochs', type=int,
+        help='Total epochs to train for')
+    train_custom_args.add_argument('--seed', type=int, required=False, default=0,
+        help='Seed for initial training process. Doesnt guarantee determinism on GPUs')
+    train_custom_args.add_argument('--gradient_accum_steps', type=int, required=False, default=8,
+        help='Gradient accumulation steps. Helps manage batch size per device. Lower it if memory is scarce')
+    train_custom_args.add_argument('--args_output_path', type=str, required=False, default=None,
+        help='Path to JSON file to save custom training argument contents into')
+    
+    # - - - Load previously-saved training arguments
+    train_presaved_args = train_subparsers.add_parser("presaved", 
+        help="Load training arguments from JSON file")
+    
+    # - - - - Register arguments
+    train_presaved_args.add_argument('args_path', type=str,
+        help='Path to JSON file containing training arguments')
+    
     # If no arguments are passed, print the usage
     if (len(argv) == 0):
         parser.parse_args(["-h"])
         exit(0)
 
     # Otherwise, parse the commands and run accordingly
-    args = parser.parse_args()
-    selected_command = args.selected_command
+    args = vars(parser.parse_args())
+    selected_command = args["selected_command"]
+    del args["selected_command"]
     
     # Run the download and/or testing of the pre-trained model
-    args = vars(args)
-    del args["selected_command"]
-
     if selected_command == "download":
         download_test_model(**args)
     elif selected_command == "evaluate":
         eval_model(**args)
+    elif selected_command == "train":
+        training_argument_source = args["training_argument_source"]
+        del args["training_argument_source"]
+
+        if training_argument_source == "custom":
+            train_model_custom_args(**args)
+        elif training_argument_source == "presaved":
+            train_model_presaved_args(**args)
+        else:
+            print(f"Unrecognized training argument source {training_argument_source}", file=stderr)
+            exit(-1)
+    
     else:
-        print(f"Unrecognized commad {selected_command}", file=stderr)
+        print(f"Unrecognized command {selected_command}", file=stderr)
         exit(-1)
 
 if __name__ == "__main__":
--- fine_tuning/commands.py
+++ fine_tuning/commands.py
@@ -141,3 +141,201 @@ def eval_model(model_path: str, dataset_dir: str, train_args_path: str, output_d
     except Exception as err:
         print(f"Unable to collect metrics on test split due to error: {err}", file=stderr)
         raise Exception("Invalid metric collection")
+
+# Use training arguments
+from transformers import TrainingArguments
+
+# Construct trainer
+from .model_trainer import create_trainer
+
+# Train model
+from .model_training import train_model
+
+# Collect training metrics
+from .model_evaluation import collect_training_metrics
+
+# Finetuning the model on a training and validation dataset
+def train_model(
+    model_path: str, dataset_dir: str, output_dir: str,
+    training_args: TrainingArguments, stats_dir: str | None = None
+):
+    # Validate and expand paths
+    assert isinstance(model_path, str), "Local model directory path should be a string"
+    model_path = path.expanduser(model_path)
+    assert path.isdir(model_path), "Missing directory for model"
+
+    assert isinstance(dataset_dir, str), "Local dataset directory path should be a string"
+    dataset_dir = path.expanduser(dataset_dir)
+    assert path.isdir(dataset_dir), "Missing directory for dataset"
+
+    assert stats_dir is None or isinstance(stats_dir, str), "When provided, training statistics output directory should be a string"
+    stats_dir = path.expanduser(stats_dir)
+
+    assert isinstance(output_dir, str), "Output directory should be a string"
+    output_dir = path.expanduser(output_dir)
+
+    # Validate training arguments
+    assert isinstance(training_args, TrainingArguments), "Invalid training arguments"
+
+    # Load model
+    print(f"Loading model and tokenizer from {model_path}...")
+
+    try:
+        model, tokenizer = load_model(model_path)
+    except Exception as err:
+        print(f"Unable to load model from '{model_path}': {err}", file=stderr)
+        raise Exception("Invalid model loading")
+    
+    print("Model and tokenizer loaded succesfully!")
+
+    # Load training and validation datasets
+    print(f"Loading training and validation dataset splits from {dataset_dir}...")
+
+    try:            
+        train_dataset = DecompilationDataset("train.csv", dataset_dir, tokenizer)
+        validate_dataset = DecompilationDataset("validation.csv", dataset_dir, tokenizer)
+    except Exception as err:
+        print(f"Unable to load training and validation datasets: {err}", file=stderr)
+        raise Exception("Invalid datasets")
+    
+    print(f"Training and validation dataset splits loaded succesfully!")
+
+    # Create training manager / trainer
+    print(f"Creating trainer instance with provided training arguments and dataset splits...")
+
+    try:
+        trainer = create_trainer(model, tokenizer, train_dataset, validate_dataset, training_args)
+    except Exception as err:
+        print(f"Unable to initialize trainer instance: {err}", file=stderr)
+        raise Exception("Invalid trainer")
+    
+    print(f"Trainer instance initialized succesfully!")
+
+    # Begin training
+    print(f"Booting up training...")
+
+    try:
+        train_model(trainer, output_dir)
+    except Exception as err:
+        print(f"An unhandled error ocurred while training the model: {err}", file=stderr)
+        raise Exception("Invalid training")
+    
+    print(f"Training finished!")
+
+    # If specified, collect training statistics 
+    if stats_dir is None:
+        print("No training statistics output directory specified. Skipping training stats recollection.")
+    else:
+        print(f"Recollecting training stats into {stats_dir}...")
+
+        try:
+            collect_training_metrics(trainer, stats_dir)
+            print(f"Training stats recollected succesfully!")
+        except Exception as err:
+            print(f"Unable to generate training statistics: {err}", file=stderr)
+
+    print("All done!")
+
+# Construct and/or save custom training arguments
+from .model_trainer import create_training_args, save_training_arguments
+
+# Finetuning the model on a training and validation dataset, with custom arguments
+def train_model_custom_args(
+    model_path: str, dataset_dir: str, output_dir: str,
+    checkpoints_dir: str, training_epochs: int, 
+    seed: int, gradient_accum_steps: int,
+    stats_dir: str | None = None,
+    args_output_path: str | None = None
+):
+    # Validate and expand paths
+    assert isinstance(model_path, str), "Local model directory path should be a string"
+    model_path = path.expanduser(model_path)
+    assert path.isdir(model_path), "Missing directory for model"
+
+    assert isinstance(dataset_dir, str), "Local dataset directory path should be a string"
+    dataset_dir = path.expanduser(dataset_dir)
+    assert path.isdir(dataset_dir), "Missing directory for dataset"
+
+    assert isinstance(output_dir, str), "Output directory should be a string"
+    output_dir = path.expanduser(output_dir)
+
+    assert stats_dir is None or isinstance(stats_dir, str), "When provided, training statistics output directory should be a string"
+    stats_dir = path.expanduser(stats_dir) if stats_dir is not None else None
+
+    # Validate provided training hyperparameters
+    # ... Checkpoints directory path
+    assert isinstance(checkpoints_dir, str), "Checkpoints directory path should be a string"
+    checkpoints_dir = path.expanduser(checkpoints_dir)
+    assert path.isdir(checkpoints_dir), "Missing directory for checkpoints"
+    # ... Arguments output directory path
+    assert args_output_path is None or isinstance(args_output_path, str), "When provided, training arguments output path should be a string"
+    args_output_path = path.expanduser(args_output_path)
+    # ... Numerical hyperparameters
+    assert isinstance(training_epochs, int) and training_epochs > 0, "Training epochs should be a positive integer"
+    assert isinstance(seed, int), "Training seed must be an integer"
+    assert isinstance(gradient_accum_steps, int) and gradient_accum_steps > 0, "Gradient accumulation steps should be a positive integer"
+
+    # Create training arguments
+    print("Initializing training arguments...")
+    try:
+        training_args = create_training_args(checkpoints_dir, training_epochs, seed, gradient_accum_steps)
+    except Exception as err:
+        print(f"Unable to create training arguments: {err}", file=stderr)
+        raise Exception("Invalid training arguments")
+    
+    print("Training arguments initialized!")
+
+    # If specified, save them for later use
+    if args_output_path is not None:
+        print("Saving training arguments...")
+
+        try:
+            save_training_arguments(training_args, args_output_path)
+        except Exception as err:
+            print(f"Unable to save training arguments: {err}", file=stderr)
+            raise Exception("Invalid training arguments serialization")
+
+        print("Training arguments saved!")
+    else:
+        print("No output path for training arguments specified, skipping save.")
+
+    # Carry on training the model
+    train_model(model_path, dataset_dir, output_dir, training_args, stats_dir)
+
+# Finetuning the model on a training and validation dataset, with pre-saved arguments
+def train_model_presaved_args(
+    model_path: str, dataset_dir: str, output_dir: str,
+    args_path: str, stats_dir: str | None = None
+):
+    # Validate and expand paths
+    assert isinstance(model_path, str), "Local model directory path should be a string"
+    model_path = path.expanduser(model_path)
+    assert path.isdir(model_path), "Missing directory for model"
+
+    assert isinstance(dataset_dir, str), "Local dataset directory path should be a string"
+    dataset_dir = path.expanduser(dataset_dir)
+    assert path.isdir(dataset_dir), "Missing directory for dataset"
+
+    assert isinstance(output_dir, str), "Output directory should be a string"
+    output_dir = path.expanduser(output_dir)
+
+    assert stats_dir is None or isinstance(stats_dir, str), "When provided, training statistics output directory should be a string"
+    stats_dir = path.expanduser(stats_dir) if stats_dir is not None else None
+
+    # Validate provided training path
+    assert isinstance(args_path, str), "Training arguments path should be a string"
+    args_path = path.expanduser(args_path)
+    assert path.isfile(args_path), "Missing training argument file"
+
+    # Load training arguments from disk
+    print(f"Loading training arguments from {args_path}...")
+    try:
+        training_args = load_training_args(args_path)
+    except Exception as err:
+        print(f"Unable to load training arguments: {err}", file=stderr)
+        raise Exception("Invalid training arguments")
+    
+    print("Training arguments loaded succesfully!")
+
+    # Carry on training the model
+    train_model(model_path, dataset_dir, output_dir, training_args, stats_dir)
